7d86fc98feaf07e10b59d653c1dc393fe3bb528f
Curtis-F <fullman1188@gmail.com>
2025-04-07 18:56:58 -0400

    added loading and error pages

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,6 +1,7 @@
 import { Pool } from '@neondatabase/serverless';
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
+import { resolve } from 'path';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -20,16 +21,18 @@ export async function connectToDB() {
 export async function getPosts() {
     try {
         noStore();
+        await new Promise((resolve) => setTimeout(resolve, 500));
         const data = await sql`SELECT * FROM posts`;
         console.log(data.rows)
 
         // Map and transform the fields to match the expected type
-        return data.rows.map((row) => ({
-            id: row.post_id?.toString() || row.id?.toString() || 'N/A',  // Ensure string ID
-            title: row.title || 'Untitled',
-            content: row.body || row.content || 'No content available',
-            date: row.created_at || row.date || new Date().toISOString()
-        }));
+        // return data.rows.map((row) => ({
+        //     id: row.post_id?.toString() || row.id?.toString() || 'N/A',  // Ensure string ID
+        //     title: row.title || 'Untitled',
+        //     content: row.body || row.content || 'No content available',
+        //     date: row.created_at || row.date || new Date().toISOString()
+        // }));
+        return data.rows;
     } catch (error) {
         console.error('Error getting posts', error);
         return [];
